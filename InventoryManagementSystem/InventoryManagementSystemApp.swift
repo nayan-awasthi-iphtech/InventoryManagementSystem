65cfcfd7791d6cfd6666dc6bebdc6bf425a582c4
nayan-awasthi-iphtech <nayan.awasthi@iphtechnologies.com>
2026-08-05 12:51:40 +0530

    Initial Commit

---
+++ InventoryManagementSystem/InventoryManagementSystemApp.swift
@@ -0,0 +1,21 @@
+//
+//  InventoryManagementSystemApp.swift
+//  InventoryManagementSystem
+//
+//  Created by iPHTech 30 on 05/08/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct InventoryManagementSystemApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
